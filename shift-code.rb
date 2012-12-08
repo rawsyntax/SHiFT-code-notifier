4ce1230691d777100e8070aa6b353c28f29d0159
Eric Himmelreich <eric@rawsyntax.com>
2012-12-08 09:29:52 -0500

    do not die when internet connection goes down, instead try again

--- shift-code.rb
+++ shift-code.rb
@@ -4,6 +4,47 @@ require 'tweetstream'
 require 'pony'
 require 'yaml'
 
+def watch_twitter(cfg)
+  puts "started watching twitter"
+
+  # gearboxsoftware user id 16567106
+  # duvalmagic id 8369072
+  # echocasts id 846328884
+  # example code CBKBB-RSJZH-RTBJJ-3TT3T-5RXZC
+  TweetStream::Client.new.follow(16567106, 8369072, 846328884) do |status|
+    # skip replies to user, the API's value of reply_to is inconsistent
+    next if status.text =~ /@\S+/
+
+    # shift codes typically expire in 3 hours
+    three_hours_ago = Time.now.utc - 10800
+
+    if status.text =~ /\w{5}-\w{5}-\w{5}-\w{5}-\w{5}/ &&
+        status.created_at > three_hours_ago &&
+        status.text.downcase.include?(cfg[:shift_type].downcase)
+      puts "mailing: #{status.text}"
+      Pony.
+        mail({
+               :to => cfg[:email][:to],
+               :cc => cfg[:email][:cc],
+               :via => :smtp,
+               :subject => "SHiFT Codes",
+               :body => status.text + "\n\n also check http://www.facebook.com/borderlandsgame",
+               :via_options => {
+                 :address              => 'smtp.gmail.com',
+                 :port                 => '587',
+                 :enable_starttls_auto => true,
+                 :user_name            => cfg[:email][:user],
+                 :password             => cfg[:email][:password],
+                 :authentication       => :plain,
+                 :domain               => "localhost.localdomain"
+               }
+             })
+    else
+      puts "NO MATCH #{status.text}"
+    end
+  end
+end
+
 cfg =
   begin
     YAML.load_file("config.yml")
@@ -17,40 +58,11 @@ TweetStream.configure do |config|
   config.auth_method = :basic
 end
 
-# gearboxsoftware user id 16567106
-# duvalmagic id 8369072
-# echocasts id 846328884
-# example code CBKBB-RSJZH-RTBJJ-3TT3T-5RXZC
-TweetStream::Client.new.follow(16567106, 8369072, 846328884) do |status|
-  # skip replies to user, the API's value of reply_to is inconsistent
-  next if status.text =~ /@\S+/
-
-  # shift codes typically expire in 3 hours
-  three_hours_ago = Time.now.utc - 10800
-
-  if status.text =~ /\w{5}-\w{5}-\w{5}-\w{5}-\w{5}/ &&
-      status.created_at > three_hours_ago &&
-      status.text.downcase.include?(cfg[:shift_type].downcase)
-    puts "mailing: #{status.text}"
-    Pony.
-      mail({
-             :to => cfg[:email][:to],
-             :cc => cfg[:email][:cc],
-             :via => :smtp,
-             :subject => "SHiFT Codes",
-             :body => status.text + "\n\n also check http://www.facebook.com/borderlandsgame",
-             :via_options => {
-               :address              => 'smtp.gmail.com',
-               :port                 => '587',
-               :enable_starttls_auto => true,
-               :user_name            => cfg[:email][:user],
-               :password             => cfg[:email][:password],
-               :authentication       => :plain,
-               :domain               => "localhost.localdomain"
-             }
-           })
-  else
-    puts "NO MATCH #{status.text}"
+while(1)
+  begin
+    watch_twitter(cfg)
+  rescue EventMachine::ConnectionError => e
+    puts e
+    sleep 5
   end
 end
-
